7023dd136c8dc81387e7f1c99268b13083066069
André Luiz <andreluiz.ral@gmail.com>
2025-03-04 14:03:42 -0300

    Criando inimigo, animação e chase; criando gridmap; criando HUD; criando animações do personagem

--- jogo-3d/Scripts/player.gd
+++ jogo-3d/Scripts/player.gd
@@ -1,28 +1,91 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 200.0
+const JUMP_VELOCITY = 8.0
+@onready var animator = get_node("AnimationPlayer") as AnimationPlayer
 
+@export var view: Node3D
+@export var health:= 3
+var gravity = 0
+var movement_velocity: Vector3
+var rotation_direction: float
+var knockbacked:= false
+
+@onready var coins_container: HBoxContainer = $HUD/coins_container
+var coins = 0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	if not knockbacked:
+		handle_input(delta)
+	apply_gravity(delta)
+	jump(delta)
+	handle_animations()
+	
+	var applied_velocity: Vector3
+	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+	
+	velocity = applied_velocity
+	
 	move_and_slide()
+	
+	
+func handle_input(delta):
+	var input:= Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_backward")
+	
+	input = input.rotated(Vector3.UP, view.rotation.y).normalized()
+	if not knockbacked:
+		velocity = input * SPEED * delta
+		if Vector2(velocity.z, velocity.x).length() > 0:
+			rotation_direction = Vector2(velocity.z, velocity.x).angle()
+	
+		rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+	
+func handle_animations():
+	if is_on_floor():
+		if abs(velocity.x) > 1 or abs(velocity.z) > 1:
+			animator.play("Run", 0.3)
+		else:
+			animator.play("Idle", 0.3)
+	else:
+		animator.play("Jump", 0.3)
+		
+	if knockbacked:
+		animator.play("Fall", 0.3)
+		
+	if not is_on_floor() and gravity > 2:
+		animator.play("Fall", 0.3)
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		gravity += 25 * delta
+	
+func jump(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		gravity = -JUMP_VELOCITY
+		
+	if gravity > 0 and is_on_floor():
+		gravity = 0
+
+func knockback(impact_point: Vector3, force: Vector3) -> void:
+	coins_container.update_life(health)
+	force.y = abs(force.y)
+	velocity = force.limit_length(15.0)
+
+func _on_hurtbox_body_entered(body: Node3D) -> void:
+	var body_collision = (body.global_position - global_position)
+	var force = -body_collision
+	health -= 1
+	force *= 10.0
+	gravity = -5.0
+	knockback(body_collision, force)
+	knockbacked = true
+	await get_tree().create_timer(0.3).timeout
+	knockbacked = false
+	
+func collect_coins():
+	coins += 1
+	coins_container.update_coin(coins)
